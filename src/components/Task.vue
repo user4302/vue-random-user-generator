0870a620c35e3d7517036b013e3be125f897e3d9
user4302 <4ngelo@outlook.com>
2022-09-09 19:07:26 +0530

    add reminder indicator

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class='task'>
+    <div :class="[task.reminder ? 'reminder' : '', 'task']">
         <h3>
-            {{task.text}} <i class='fas fa-times'></i>
+            {{task.text}} <i class="fas fa-times"></i>
         </h3>
         <p>{{task.day}}</p>
     </div>
